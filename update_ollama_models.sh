d96f3908084f221180541329664259bbb818ab22
Rick Dawson <dawsora@gmail.com>
2024-09-16 11:52:09 -0400

    added local option to update_ollama_models.sh

--- update_ollama_models.sh
+++ update_ollama_models.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
 # update_llm.sh
 
-# Retrieves the list of LLMs installed in the Docker container
+update_docker() {
+  # Retrieves the list of LLMs installed in the Docker container
 llm_list=$(docker exec ollama ollama list | tail -n +2 | awk '{print $1}')
 
 # Loop over each LLM to update it
 for llm in $llm_list; do
   docker exec ollama ollama pull $llm
 done
+}
+
+update_local() {
+  # Retrieves the list of LLMs installed locally
+llm_list=$(ollama list | tail -n +2 | awk '{print $1}')
+
+# Loop over each LLM to update it
+for llm in $llm_list; do
+  ollama pull $llm
+done
+}
+
+for getopts "dhl" opt; do
+  case ${opt} in
+    d)
+      update_docker
+      ;;
+    h )
+      echo "Usage: update_llm.sh"
+      echo "Updates all LLMs installed in the Docker container"
+      exit 0
+      ;;
+    l)
+      update_local
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+# If no options are provided, update the LLMs installed locally
+if [ $# -eq 0 ]; then
+  update_local
+fi
+
+echo "All LLMs updated successfully"
+
